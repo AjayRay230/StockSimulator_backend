55a45fd5475d95a523a8707734819ca599c16ade
royajaygit <ajayray2409@gmail.com>
2025-08-23 02:55:35 +0530

    V2 insert two users, V3 add UNIQUE constraint and insert ajayroy

--- src/main/resources/db/migration/V3__add_ajayroy_user.sql
+++ src/main/resources/db/migration/V3__add_ajayroy_user.sql
@@ -1,10 +1,4 @@
 INSERT INTO users (username, firstname, lastname, password, email, amount, role)
 VALUES
-    ('ajayroy','AJAY','ROY','$2a$12$lNnrPojpUGBi7ShsZEY6weo260eN6XspeYqjMUWzNuQtBpxSAH3/m','AjayRy7439@gmail.com',1000000,'ADMIN')
-    ON CONFLICT (username) DO UPDATE
-                                  SET firstname = EXCLUDED.firstname,
-                                  lastname  = EXCLUDED.lastname,
-                                  password  = EXCLUDED.password,
-                                  email     = EXCLUDED.email,
-                                  amount    = EXCLUDED.amount,
-                                  role      = EXCLUDED.role;
+    ('ajayroy','AJAY','ROY','$2a$12$lNnrPojpUGBi7ShsZEY6weo260eN6XspeYqjMUWzNuQtBpxSAH3/m','AjayRy7439@gmail.com',1000000,'ADMIN');
+
