6dcd04386b00ef1232f6072d7631d45dc1f77795
royajaygit <ajayray2409@gmail.com>
2025-08-22 15:20:05 +0530

    Fix Flyway configuration for PostgreSQL 17.6 support

--- src/main/java/org/ajay/stockSimulator/Repo/TransactionRepo.java
+++ src/main/java/org/ajay/stockSimulator/Repo/TransactionRepo.java
@@ -18,24 +18,7 @@ import java.util.List;
 
 @Repository
 public interface TransactionRepo extends JpaRepository<Transaction,Long> {
-//    @Query("SELECT t.stocksymbol, COUNT(t) as tradeCount " +
-//            "FROM Transaction t " +
-//            "GROUP BY t.stocksymbol " +
-//            "ORDER BY tradeCount DESC")
-//    List<Object[]> findTopStocks();
 
-
-
-//    @Query(value = "SELECT * FROM transactions ORDER BY `timestamp` DESC LIMIT :limit", nativeQuery = true)
-//    List<Transaction> findAllByOrderByTimestampDesc(Pageable pageable);
-//
-//    // Top traded stocks by transaction count
-//    @Query(value = "SELECT stock_symbol, COUNT(*) as trade_count " +
-//            "FROM transactions GROUP BY stock_symbol " +
-//            "ORDER BY trade_count DESC LIMIT :limit", nativeQuery = true)
-//    List<Object[]> findTopTradedStocks(@Param("limit") int limit);
-//    @Query("SELECT COUNT(DISTINCT t.user.userId) FROM Transaction t WHERE t.timestamp BETWEEN :start AND :end")
-//    long countDistinctUsersByTimestampBetween(LocalDateTime start, LocalDateTime end);
 
     List<Transaction> findByUser_UserId(Long id);
 
@@ -50,9 +33,11 @@ public interface TransactionRepo extends JpaRepository<Transaction,Long> {
     List<Object[]> findTopStocks();
 
     // Trades Executed Today
-    @Query("SELECT t.stocksymbol, COUNT(t), MAX(t.timestamp) FROM Transaction t " +
-            "WHERE DATE(t.timestamp) = CURRENT_DATE " +
-            "GROUP BY t.stocksymbol ORDER BY COUNT(t) DESC")
+    @Query("SELECT t.stocksymbol, COUNT(t), MAX(t.timestamp) " +
+            "FROM Transaction t " +
+            "WHERE t.timestamp >= CURRENT_DATE AND t.timestamp < CURRENT_DATE + 1 " +
+            "GROUP BY t.stocksymbol " +
+            "ORDER BY COUNT(t) DESC")
     List<Object[]> findTradesExecuted();
 
     // Active Traders
@@ -65,12 +50,7 @@ public interface TransactionRepo extends JpaRepository<Transaction,Long> {
 
     Page<Transaction> findAllByOrderByTimestampDesc(Pageable pageable);
 
-//    interface UserRepository extends org.springframework.data.repository.Repository<User, Long> {
-//
-//
-//
-//
-//       List<Transaction> findByUserId(Long userId);
+
 
 
 }
