70b51298233ef1764566c51cc3cc3a442074bd25
royajaygit <ajayray2409@gmail.com>
2025-08-23 00:12:45 +0530

    fix crocs problem

--- src/main/java/org/ajay/stockSimulator/Configuration/SpringConfiguration.java
+++ src/main/java/org/ajay/stockSimulator/Configuration/SpringConfiguration.java
@@ -80,7 +80,9 @@ public AuthenticationManager authenticationManager(AuthenticationConfiguration c
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of(
+                "https://stockswift.netlify.app",
+                "http://localhost:5173"   ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
