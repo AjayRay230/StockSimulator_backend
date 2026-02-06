068057dc56c1a2ece8519aa7e79b592959df3976
royajaygit <ajayray2409@gmail.com>
2026-02-06 17:51:29 +0530

    fixing table v5

--- src/main/resources/db/migration/V5__create_password_reset_token_fix.sql
+++ src/main/resources/db/migration/V5__create_password_reset_token_fix.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS password_reset_token (
-                                                    id BIGSERIAL PRIMARY KEY,
-                                                    token VARCHAR(255) NOT NULL UNIQUE,
+    id BIGSERIAL PRIMARY KEY,
+    token VARCHAR(255) NOT NULL UNIQUE,
     user_id BIGINT NOT NULL,
     expiry_date TIMESTAMP NOT NULL,
     CONSTRAINT fk_password_reset_user
