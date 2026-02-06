c17f290ecf73a6d3e8c67fb1cc8e627dacddbb1f
royajaygit <ajayray2409@gmail.com>
2026-02-06 23:56:37 +0530

    change the mail sending service

--- src/main/java/org/ajay/stockSimulator/service/EmailService.java
+++ src/main/java/org/ajay/stockSimulator/service/EmailService.java
@@ -1,24 +1,61 @@
 package org.ajay.stockSimulator.service;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
+import com.sendgrid.*;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
 public class EmailService {
 
-    private final JavaMailSender mailSender;
+    private final SendGrid sendGrid;
+    private final String fromEmail;
+    private final String fromName;
+
+    public EmailService(
+            @Value("${sendgrid.api.key}") String apiKey,
+            @Value("${app.mail.from}") String fromEmail,
+            @Value("${app.mail.from-name}") String fromName
+    ) {
+        this.sendGrid = new SendGrid(apiKey);
+        this.fromEmail = fromEmail;
+        this.fromName = fromName;
+    }
 
     public void sendResetLink(String email, String link) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(email);
-        message.setSubject("Password Reset");
-        message.setText(
-                "Click the link to reset your password:\n\n" + link +
-                        "\n\nThis link expires in 15 minutes."
+
+        Email from = new Email(fromEmail, fromName);
+        Email to = new Email(email);
+
+        String subject = "Password Reset";
+
+        Content content = new Content(
+                "text/html",
+                """
+                <p>Click the link below to reset your password:</p>
+                <p><a href="%s">Reset Password</a></p>
+                <p>This link expires in <b>15 minutes</b>.</p>
+                """.formatted(link)
         );
-        mailSender.send(message);
+
+        Mail mail = new Mail(from, subject, to, content);
+
+        Request request = new Request();
+        try {
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build());
+
+            Response response = sendGrid.api(request);
+
+            if (response.getStatusCode() >= 400) {
+                throw new RuntimeException("SendGrid error: " + response.getBody());
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to send email", e);
+        }
     }
 }
