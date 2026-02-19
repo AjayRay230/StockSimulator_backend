4ecd112ebc35d6cabf515d9fca973f30350720ea
royajaygit <ajayray2409@gmail.com>
2026-02-20 01:21:32 +0530

    added findUsersWithPortfolio()   method to PortfolioItemService error 1

--- src/main/java/org/ajay/stockSimulator/DTOs/DashboardMetricsDTO.java
+++ src/main/java/org/ajay/stockSimulator/DTOs/DashboardMetricsDTO.java
@@ -1,10 +1,6 @@
 package org.ajay.stockSimulator.DTOs;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 
-@AllArgsConstructor
-@NoArgsConstructor
 public class DashboardMetricsDTO {
 
     private String symbol;
@@ -15,5 +11,30 @@ public class DashboardMetricsDTO {
     private double portfolioValue;
     private long tradesToday;
 
+    public DashboardMetricsDTO(
+            String symbol,
+            int quantity,
+            double avgBuyPrice,
+            double currentPrice,
+            double unrealizedPnL,
+            double portfolioValue,
+            long tradesToday) {
 
+        this.symbol = symbol;
+        this.quantity = quantity;
+        this.avgBuyPrice = avgBuyPrice;
+        this.currentPrice = currentPrice;
+        this.unrealizedPnL = unrealizedPnL;
+        this.portfolioValue = portfolioValue;
+        this.tradesToday = tradesToday;
+    }
+
+    public String getSymbol() { return symbol; }
+    public int getQuantity() { return quantity; }
+    public double getAvgBuyPrice() { return avgBuyPrice; }
+    public double getCurrentPrice() { return currentPrice; }
+    public double getUnrealizedPnL() { return unrealizedPnL; }
+    public double getPortfolioValue() { return portfolioValue; }
+    public long getTradesToday() { return tradesToday; }
 }
+
